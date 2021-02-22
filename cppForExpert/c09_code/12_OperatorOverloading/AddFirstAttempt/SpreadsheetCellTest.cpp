97e54e823d266df3a17092f1784c1727c8added4
bnuq <bnuq@outlook.kr>
2021-02-22 23:49:32 +0900

    초기 커밋.

---
+++ cppForExpert/c09_code/12_OperatorOverloading/AddFirstAttempt/SpreadsheetCellTest.cpp
@@ -0,0 +1,15 @@
+#include "SpreadsheetCell.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	SpreadsheetCell myCell(4), anotherCell(5);
+	SpreadsheetCell aThirdCell = myCell.add(anotherCell);
+	cout << aThirdCell.getValue() << endl;
+
+	cout << "test" << endl;
+
+	return 0;
+}
